5270e7413b6ebecaef70065ba2e6af3e9255a9e1
Patrick Davey <ghent12@gmail.com>
2022-10-16 14:07:52 -0500

    Implement player and computer choices

--- javascript.js
+++ javascript.js
@@ -1,8 +1,15 @@
 const gameBoard = (() => {
-  let _boardArray = new Array(9);
+  const gameBoardSize = () => 9;
+
+  let _boardArray = new Array(gameBoardSize());
   const claimSquare = (position, player) => {
-    _boardArray[position] = player.getSymbol();
+    _boardArray[position - 1] = player.getSymbol();
+    console.log(_boardArray)
+    let button = document.querySelector(`.play-button-${position}`);
+    button.textContent = player.getSymbol();
   }
+
+  const getSquareInfo = (position) => _boardArray[position - 1];
 
   const _createBoard = (() => {
     const htmlBody = document.getElementById('body')
@@ -10,16 +17,22 @@ const gameBoard = (() => {
           playArea.classList.add('play-area');
   for (let i = 1; i <= _boardArray.length; i++) {
       const boardButtonHolder = playArea.appendChild(document.createElement('div'));
-            boardButtonHolder.classList.add('play-button-holder-' + i);
+            boardButtonHolder.classList.add('button-holder-' + i);
       const boardButton = boardButtonHolder.appendChild(document.createElement('button'));
             boardButton.classList.add('play-button', 'play-button-' + i);
-            //boardButton.textContent = i;
             boardButton.id = 'play-button-' + i;
     }
   })();
 
+  const gameStatus = () => {
+
+  }
+
   return {
-    claimSquare
+    claimSquare,
+    getSquareInfo,
+    gameStatus,
+    gameBoardSize
   };
 })();
 
@@ -33,7 +46,49 @@ const Player = (symbol) => {
 }
 
 const gameFlow = (() => {
+  const _player = Player('X');
+  const _computer = Player('O');
+  //const test = console.log(`Clicked ${boardButton.id}`)
+  const attemptClaim = (playButton) => {
+    console.log('playButton used for attemptClaim is ' + playButton)
+    const position = gameBoard.getSquareInfo(playButton);
+    console.log('position inside attemptClaim is ' + position)
+    if (position == undefined) {
+      console.log(_player)
+      gameBoard.claimSquare(playButton, _player)
+      if (checkGameStatus()) {
 
+      } else {
+        computerTurn();
+      }
+    } else {
+      console.log('No can do.')
+    }
+  }
+
+  const computerTurn = () => {
+    let availablePositions = [];
+    for (let i = 1; i <= gameBoard.gameBoardSize(); i++) {
+      if (gameBoard.getSquareInfo(i) == undefined) {
+        availablePositions.push(i);
+        //console.log(`hi ${i}`);
+      }
+    }
+    console.log(availablePositions);
+    const computerChoice = parseInt(Math.random() * availablePositions.length);
+    console.log(`index ${computerChoice} results in availablePositions choice of ${availablePositions[computerChoice]}`);
+    gameBoard.claimSquare(availablePositions[computerChoice], _computer)
+  }
+
+  const checkGameStatus = () => {
+
+  }
+
+  return {
+    attemptClaim,
+    computerTurn,
+    checkGameStatus
+  }
 })();
 
 const displayController = (() => {
@@ -47,7 +102,12 @@ const displayController = (() => {
   })();
 
   const _initializeButtonClicks = (() => {
-    //const 
+    const boardButtons = Array.prototype.slice.call(document.querySelectorAll('.play-button'));
+    console.log(boardButtons);
+    for (let i = 0; i < boardButtons.length; i++) {
+      let button = boardButtons[i];
+      button.addEventListener('click', gameFlow.attemptClaim.bind(button, i + 1))
+    }
   })();
 
   return {
